b262c585caaa8d2f27738b923916022ad9a6c3ab
deepikabala14 <deepikabala14>
2023-10-18 01:53:38 -0400

    Added Logging and Exception

--- setup.py
+++ setup.py
@@ -1,7 +1,6 @@
 from setuptools import find_packages,setup
 from typing import List
 
-
 HYPEN_E_DOT= '-e .'
 def get_requirements(file_path:str)->List[str]:
     '''
@@ -11,24 +10,10 @@ def get_requirements(file_path:str)->List[str]:
     with open(file_path) as file_obj:
         requirements=file_obj.readlines()
         requirements = [req.replace("/n"," ") for req in requirements]
-
         if HYPEN_E_DOT in requirements:
             requirements.remove(HYPEN_E_DOT)
     
     return requirements
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 setup(
     name='mlproject',
@@ -37,5 +22,4 @@ setup(
     author_email ='balasubramanian.de@northeastern.edu',
     packages=find_packages(),
     install_requires=get_requirements('requirements.txt')
-
 )
